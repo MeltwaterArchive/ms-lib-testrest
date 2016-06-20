365481d12601126376e16a1da6c09e3f73a90813
Nathan Macnamara <nathan.macnamara@datasift.com>
2016-06-20 21:09:46 +0100

    Remove debugging line

--- src/Context/RestContext.php
+++ src/Context/RestContext.php
@@ -577,7 +577,6 @@ class RestContext implements ApiClientAwareContext
         Assertions::assertGreaterThanOrEqual(count($etalon), count($actual));
         foreach ($etalon as $key => $needle) {
             Assertions::assertArrayHasKey($key, $actual);
-            var_dump($actual[$key]);
             if ($etalon[$key] != '*') {
                 Assertions::assertEquals($etalon[$key], $actual[$key]);
             }
